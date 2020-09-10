66f84bfb28d77b1c0716f509420b4c48218703db
ian-yitzhak <onyangoian71@gmail.com>
2020-09-10 11:51:34 +0300

    first commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,8 +129,4 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
